771b4f9e05ddcdaefce6fc28b8d6557aee12143c
Aniket Yadav <aniketyadav690@gmail.com>
2024-07-27 22:22:37 +0530

    update: Images added

--- src/components/LeaderBoard/LeaderBoard.tsx
+++ src/components/LeaderBoard/LeaderBoard.tsx
@@ -2,6 +2,16 @@ import React, { useState, useEffect } from "react";
 import { LeaderBoardConatiner, Heading } from "./LeaderBoardStyle";
 import profile from "../../assets/profile-icon-upload.png";
 import Player from "../Player/Player";
+import Joy from "../../assets/Joy.png";
+import Jone from "../../assets/Jone.png";
+import Quinn from "../../assets/Quinn.png";
+import Victoria from "../../assets/Victoria.png";
+import Sheena from "../../assets/Sheena.png";
+import Charlene from "../../assets/Charlene.png";
+import Leonababy from "../../assets/Leonababy.png";
+import Sunny from "../../assets/Sunny.png";
+import InWord from "../../assets/Inword.png";
+import Dophine from "../../assets/Dophine.png";
 
 interface PlayerData {
   userID: string;
@@ -11,16 +21,16 @@ interface PlayerData {
 }
 
 const initialPlayers: PlayerData[] = [
-  { userID: "u-1", displayName: "Jone", picture: profile, score: 46200 },
-  { userID: "u-2", displayName: "Victoria", picture: profile, score: 46200 },
-  { userID: "u-3", displayName: "Joy", picture: profile, score: 46200 },
-  { userID: "u-4", displayName: "Quinn", picture: profile, score: 33400 },
-  { userID: "u-5", displayName: "Sheenalo", picture: profile, score: 46200 },
-  { userID: "u-6", displayName: "Charlene", picture: profile, score: 30800 },
-  { userID: "u-7", displayName: "LeonaBaby", picture: profile, score: 22300 },
-  { userID: "u-8", displayName: "Sunny", picture: profile, score: 17800 },
-  { userID: "u-9", displayName: "ImWord", picture: profile, score: 17300 },
-  { userID: "u-10", displayName: "Dophine", picture: profile, score: 15400 },
+  { userID: "u-1", displayName: "Jone", picture: Jone, score: 46200 },
+  { userID: "u-2", displayName: "Victoria", picture: Victoria, score: 46200 },
+  { userID: "u-3", displayName: "Joy", picture: Joy, score: 46200 },
+  { userID: "u-4", displayName: "Quinn", picture: Quinn, score: 33400 },
+  { userID: "u-5", displayName: "Sheenalo", picture: Sheena, score: 46200 },
+  { userID: "u-6", displayName: "Charlene", picture: Charlene, score: 30800 },
+  { userID: "u-7", displayName: "LeonaBaby", picture: Leonababy, score: 22300 },
+  { userID: "u-8", displayName: "Sunny", picture: Sunny, score: 17800 },
+  { userID: "u-9", displayName: "ImWord", picture: InWord, score: 17300 },
+  { userID: "u-10", displayName: "Dophine", picture: Dophine, score: 19400 },
 ];
 
 const LeaderBoard: React.FC = () => {
